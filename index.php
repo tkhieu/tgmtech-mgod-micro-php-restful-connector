60329aecd9720d691bf8b78951a31cb24c10456f
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-16 17:27:39 +0700

    Hoàn thành phương thức POST/item

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'Slim/Slim.php';
 require_once 'idiorm.php';
 
@@ -17,45 +18,42 @@ ORM::configure('id_column', 'id');
 
 \Slim\Slim::registerAutoloader();
 
-$app = new \Slim\Slim(array('mode'=>'development', 'debug'=>'true'));
-$app->get('/hello/:name', function ($name) {
-    echo "Hello, $name";
-});
+$app = new \Slim\Slim(array('mode' => 'development', 'debug' => 'true'));
 
-// POST:/hello/
-$app->post('/hello/', function (){
-    $item = ORM::for_table('item_info')->create();
+// POST:/item/
+$app->post('/item/', function () {
+            $item = ORM::for_table('item_info')->create();
 
-    // Chuyển biến từ global POST sang biến địa phương
-    // Chuyển để dễ dàng control data type thay vì gán thẳng
-    if($_POST != null){
-        $name =  $_POST['name'];
-//        $phone = $_POST['phone'];
-//        $address = $_POST['address'];
-//        $detail = $_POST['detail'];
-//        $username = $_POST['username'];
-//        $userid = $_POST['userid'];
-//        $situation =  $_POST['situation'];
-//        $price = $_POST['price'];
-//        $categoryname = $_POST['categoryname'];
-//        $categoryid = $_POST['categoryid'];            
-    }
-    
-    // Đẩy biến vào trong ORM entity
-    echo $name;
-    
-    $item->name = $name;
-//    $item->phone = $phone;
-//    $item->address = $address;
-//    $item->detail = $address;
-//    $item->username = $username;
-//    $item->userid = $userid;
-//    $item->situation = $situation;
-//    $item->price = $price;
-//    $item->categoryname = $categoryname;
-//    $item->categoryid = $categoryid;
-        $item->save();
-});
+            // Chuyển biến từ global POST sang biến địa phương
+            // Chuyển để dễ dàng control data type thay vì gán thẳng
+            if ($_POST != null) {
+                $name = $_POST['name'];
+                $phone = $_POST['phone'];
+                $address = $_POST['address'];
+                $detail = $_POST['detail'];
+                $username = $_POST['username'];
+                $userid = $_POST['userid'];
+                $situation = $_POST['situation'];
+                $price = $_POST['price'];
+                $categoryname = $_POST['categoryname'];
+                $categoryid = $_POST['categoryid'];
+            }
+
+            // Đẩy biến vào trong ORM entity
+            echo $name;
+
+            $item->name = $name;
+            $item->phone = $phone;
+            $item->address = $address;
+            $item->detail = $detail;
+            $item->username = $username;
+            $item->userid = $userid;
+            $item->situation = $situation;
+            $item->price = $price;
+            $item->categoryname = $categoryname;
+            $item->categoryid = $categoryid;
+            return $item->save();
+        });
 $app->run();
 /*
  * To change this template, choose Tools | Templates
