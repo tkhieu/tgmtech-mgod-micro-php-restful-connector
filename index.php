f1d66c033a2f9cd4da4b9e9d6d9b67836167beff
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-16 23:52:24 +0700

    Add comment

--- index.php
+++ index.php
@@ -21,77 +21,76 @@ ORM::configure('id_column', 'id');
 // Redbean Config
 R::setup('mysql:host=localhost;dbname=test-slim', 'root', '123456');
 
-//R::freeze(true);
-
-
+// Đăng ký Slim với request handle
 \Slim\Slim::registerAutoloader();
-
+// New một Slim App
 $app = new \Slim\Slim(array('mode' => 'development', 'debug' => 'false'));
 $app->response()->header('Content-Type', 'application/json');
-// POST:/item/
+
+// POST /item/
 $app->post('/item/', function () {
             $item = ORM::for_table('item_info')->create();
-            
+
             try {
                 if ($_POST != null) {
 
-                if ($_POST['name'] != null) {
-                    $name = $_POST['name'];
-                    $item->name = $name;
-                }
+                    if ($_POST['name'] != null) {
+                        $name = $_POST['name'];
+                        $item->name = $name;
+                    }
 
-                if ($_POST['phone'] != null) {
-                    $phone = $_POST['phone'];
-                    $item->phone = $phone;
-                }
+                    if ($_POST['phone'] != null) {
+                        $phone = $_POST['phone'];
+                        $item->phone = $phone;
+                    }
 
-                if ($_POST['address'] != null) {
-                    $address = $_POST['address'];
-                    $item->address = $address;
-                }
+                    if ($_POST['address'] != null) {
+                        $address = $_POST['address'];
+                        $item->address = $address;
+                    }
 
 
-                if ($_POST['detail'] != null) {
-                    $detail = $_POST['detail'];
-                    $item->detail = $detail;
-                }
+                    if ($_POST['detail'] != null) {
+                        $detail = $_POST['detail'];
+                        $item->detail = $detail;
+                    }
 
-                if ($_POST['username'] != null) {
-                    $username = $_POST['username'];
-                    $item->username = $username;
-                }
+                    if ($_POST['username'] != null) {
+                        $username = $_POST['username'];
+                        $item->username = $username;
+                    }
 
-                if ($_POST['userid'] != null) {
-                    $userid = $_POST['userid'];
-                    $item->userid = $userid;
-                }
+                    if ($_POST['userid'] != null) {
+                        $userid = $_POST['userid'];
+                        $item->userid = $userid;
+                    }
 
-                if ($_POST['situation'] != null) {
-                    $situation = $_POST['situation'];
-                    $item->situation = $situation;
-                }
+                    if ($_POST['situation'] != null) {
+                        $situation = $_POST['situation'];
+                        $item->situation = $situation;
+                    }
 
-                if ($_POST['price'] != null) {
-                    $price = $_POST['price'];
-                    $item->price = $price;
-                }
+                    if ($_POST['price'] != null) {
+                        $price = $_POST['price'];
+                        $item->price = $price;
+                    }
 
-                if ($_POST['categoryname'] != null) {
-                    $categoryname = $_POST['categoryname'];
-                    $item->categoryname = $categoryname;
-                }
+                    if ($_POST['categoryname'] != null) {
+                        $categoryname = $_POST['categoryname'];
+                        $item->categoryname = $categoryname;
+                    }
 
-                if ($_POST['categoryid'] != null) {
-                    $categoryid = $_POST['categoryid'];
-                    $item->categoryid = $categoryid;
+                    if ($_POST['categoryid'] != null) {
+                        $categoryid = $_POST['categoryid'];
+                        $item->categoryid = $categoryid;
+                    }
                 }
-            }
-            $item->status = 1;
-            $item->Aa = 1;
-            if($item->save())
-                echo 1;
-            else echo 0;
-                
+                $item->status = 1;
+                $item->Aa = 1;
+                if ($item->save())
+                    echo 1;
+                else
+                    echo 0;
             } catch (Exception $exc) {
                 echo 0;
             }
@@ -99,12 +98,10 @@ $app->post('/item/', function () {
 
             // Chuyển biến từ global POST sang biến địa phương
             // Chuyển để dễ dàng control data type thay vì gán thẳng
-            
-            
             // Đúng sẽ trả về true
         });
 
-// GET:/item/:id
+// GET /item/:id
 $app->get('/item/:id', function ($id) {
 
             if ($id == 'all') {
@@ -120,45 +117,50 @@ $app->get('/item/:id', function ($id) {
             else
                 echo $json;
         });
-
+// PUT /item/:id
 $app->put('/item/:id', function ($id) use($app) {
-    
+
             try {
                 $item = ORM::for_table('item_info')->find_one($id);
                 $data = json_decode($app->getInstance()->request()->getBody());
-            $item->name = $data->name;
+                $item->name = $data->name;
 
-            $item->name = $data->name;
-            $item->phone = $data->phone;
-            $item->address = $data->address;
-            $item->detail = $data->detail;
-            $item->username = $data->username;
-            $item->userid = $data->userid;
-            $item->situation = $data->situation;
-            $item->price = $data->price;
-            $item->categoryname = $data->categoryname;
-            $item->categoryid = $data->categoryid;
-            if ($item->save())
-                echo 1;
-            else
-                echo 0;
+                $item->name = $data->name;
+                $item->phone = $data->phone;
+                $item->address = $data->address;
+                $item->detail = $data->detail;
+                $item->username = $data->username;
+                $item->userid = $data->userid;
+                $item->situation = $data->situation;
+                $item->price = $data->price;
+                $item->categoryname = $data->categoryname;
+                $item->categoryid = $data->categoryid;
+                if ($item->save())
+                    echo 1;
+                else
+                    echo 0;
             } catch (Exception $exc) {
                 echo 0;
             }
-
-            
         });
+
+// DELETE /item/:id
 $app->delete('/item/:id', function ($id) {
 
-            $item = R::load('item_info', $id);
-            $id_before = $item->id;
-            R::trash($item);
-            $item_after = ORM::for_table('item_info')->find_one($id);
-            if ((int) $id_before == 0 && (int) $item_after == 0)
+            try {
+                $item = ORM::for_table('item_info')->find_one($id);
+                $status_before = $item->status;
+                $item->status = 0;
+                //R::trash($item);
+                $item->save();
+                $item_after = ORM::for_table('item_info')->find_one($id);
+                if ((int) $status_before == 1 && (int) $item_after->status == 0)
+                    echo 1;
+                else
+                    echo 0;
+            } catch (Exception $exc) {
                 echo 0;
-            else
-                echo 1;
-            // echo json_encode($item_after);
+            }
         });
 
 // Run the App
