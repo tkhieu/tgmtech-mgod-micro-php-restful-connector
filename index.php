e32ba314c1a0cdd33302505b2c4d6d5f0a7e4a3c
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-16 14:36:53 +0700

    Hoàn thành add Slim và Hi Slim

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
 
-echo "Hi Slim";
+$app = new \Slim\Slim(array('mode'=>'development', 'debug'=>'true'));
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, $name";
+});
+$app->run();
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
