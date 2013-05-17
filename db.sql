77c58dd38145453b27878c98fae976ae3bc596e0
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-17 16:08:33 +0700

    Cập nhật script db

--- db.sql
+++ db.sql
@@ -8,10 +8,10 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 DROP TABLE IF EXISTS `item_info`;
 CREATE TABLE `item_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `posttime` bigint(20) NOT NULL,
-  `updatetime` bigint(20) NOT NULL,
-  `topicid` bigint(20) NOT NULL,
-  `status` tinyint(4) NOT NULL,
+  `posttime` bigint(20) DEFAULT NULL,
+  `updatetime` bigint(20) DEFAULT NULL,
+  `topicid` bigint(20) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT NULL,
   `name` varchar(1024) DEFAULT NULL,
   `images` text CHARACTER SET ascii,
   `phone` varchar(15) DEFAULT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE `item_info` (
   `categoryname` varchar(100) DEFAULT NULL,
   `categoryid` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='Bảng lưu các thông tin phụ cho một lời rao trên MGOD - ver 1';
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COMMENT='Bảng lưu các thông tin phụ cho một lời rao trên MGOD - ver 1';
 
 
--- 2013-05-17 09:46:44
+-- 2013-05-17 16:07:49
