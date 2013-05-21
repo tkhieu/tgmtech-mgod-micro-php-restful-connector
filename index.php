7bd20eadec259eb29a5b1d1e246d0286a633c590
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-21 14:13:17 +0700

    Fix lỗi file conflic

--- index.php
+++ index.php
@@ -168,11 +168,8 @@ $app->get('/items/all/:page/:limit', function ($page, $limit) use($app) {
             if ($json == "{\"id\":0}")
                 echo $json_false;
             else
-<<<<<<< HEAD
                 echo "\[".$json."\]";
-=======
-                echo "[".$json."]";
->>>>>>> e0ce7824de675e1ade81e6c68418b4390b1f2b72
+
         });
 
 
