fafeb33d9843bf338b0439e63a79febbbb18bc2e
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-29 12:50:37 +0700

    Chuyển config.php sang thành Class + Add index.php

--- config.php
+++ config.php
@@ -10,11 +10,15 @@
 //$mysql_username = "root";
 //$mysql_password = "";
 
+class Config {
 
+    static public $key = "tgm-mgod";
+    static public $secret = "cjpmrJG7nRqD9NDRFRKJSwNZKZybKe69Vt8Qd8cxmCEMGxSzPvGd4u4ftUDvZSWqV9hPmcWDytmb3UxshTKgGMUB72jaed7BBPRr";
+    static public $mysql_add = "us-cdbr-east-03.cleardb.com";
+    static public $mysql_username = "b0f193c24de04b";
+    static public $mysql_password = "4d21803e";
+    static public $mysql_db = "heroku_30763ce34eaaa50";
 
-$mysql_add = "us-cdbr-east-03.cleardb.com";
-$mysql_username = "b0f193c24de04b";
-$mysql_password = "4d21803e";
-$mysql_db = "heroku_30763ce34eaaa50";
-        
+}
+
 ?>
