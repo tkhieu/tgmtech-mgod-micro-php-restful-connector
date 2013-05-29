b53f321ab83638530e7554e23880caf8ec786e93
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-05-29 12:49:55 +0700

    Add TGMToken.php, thay đổi key

--- TGMToken.php
+++ TGMToken.php
@@ -11,34 +11,38 @@
  * @author hieu
  */
 class TGMToken {
+
     //put your code here
-    
-    private $key = "GuN4fM2NjQE6wgpEwrRhhqX2kajTsnKDEUZ7TS9J75Jus5XMhK";
+
+    private $key = "tgm-mgod";
     private $secret = "cjpmrJG7nRqD9NDRFRKJSwNZKZybKe69Vt8Qd8cxmCEMGxSzPvGd4u4ftUDvZSWqV9hPmcWDytmb3UxshTKgGMUB72jaed7BBPRr";
-    
-    public function check($param) {
+
+    public static function check($param) {
         $sign = $param["sign"];
         $app_key = $param["key"];
         $timestamp = $param["timestamp"];
-        
-        if($app_key == $this->key){
-            /* @var $secret type */
-            $check = md5($this->key.$timestamp) . md5($timestamp) . md5($this->secret.$timestamp);
-            echo $check. "<br />";
-            
-            $check_md5 = md5($check);
-            echo $check_md5. "<br />";
-            
-            if($check_md5 == $sign)
-                echo "a";
-            else echo "b";
-        }
-        
-        
-    }
 
+        if ($app_key == $this->key) {
+            /* @var $secret type */
+            $check = md5($this->key . $timestamp) . md5($timestamp) . md5($this->secret . $timestamp);
+            $check_md5 = md5($check);
+            if ($check_md5 == $sign)
+                return true;
+            else
+                return false;
+        }
+    }
     
-    
+    public static function getparams() {
+        $headers = $_REQUEST;
+        
+        if ($headers['Sign'] != null && $headers['Key'] != null && $headers['Timestamp'] != null){
+            $params = array("Key"=>$headers["Key"],"Sign"=>$headers["Sign"],"Timestamp"=>$headers["Sign"]);
+            return $params;
+        }
+        return false;
+    } 
+
 }
 
 ?>
