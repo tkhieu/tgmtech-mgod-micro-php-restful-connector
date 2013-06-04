53b358453bde5bafeec8db811765e87e12575d9a
Trần Kim Hiếu <tr.kimhieu@gmail.com>
2013-06-04 16:16:09 +0700

    Push Heroku 2 test

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 // Require Autoload nhằm tự load các config và thư viện cần thiết
 require_once './autoload.php';
 
+
 // Khởi tạo Slim
 
 $app = new \Slim\Slim(array('mode' => 'production', 'debug' => 'false'));
